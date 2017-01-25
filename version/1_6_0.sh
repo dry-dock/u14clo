dde2a4530bd7df6a04a1ec6f61bb2c6ac9e713b9
Dan Sapala <dsapala@gmail.com>
2017-01-25 21:55:22 +0000

    stripping trailing whitespace

--- version/1_6_0.sh
+++ version/1_6_0.sh
@@ -3,7 +3,7 @@
 # Install Clojure 1.6.0
 echo "================= Installing Clojure-1.6.0 ==================="
 sudo wget http://repo1.maven.org/maven2/org/clojure/clojure/1.6.0/clojure-1.6.0.zip
-sudo unzip clojure-1.6.0.zip 
+sudo unzip clojure-1.6.0.zip
 mkdir $HOME/lib && cp clojure-1.6.0/clojure-1.6.0.jar $HOME/lib
 sudo rm -rf clojure-1.6.0*
 cat >/usr/local/bin/clj <<EOF
